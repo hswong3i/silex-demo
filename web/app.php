d9edb3ae5da4ffb159e682ca08d736d723c76992
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-07-15 10:19:16 +0800

    Clone index page from authbucket/oauth2.

--- web/app.php
+++ web/app.php
@@ -4,11 +4,17 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application(); 
 
+$app['debug'] = true;
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/../views',
 ));
 
-$app->get('/hello/{name}', function($name) use($app) { 
+$app->get('/', function() use ($app) {
+    return $app['twig']->render('index.html.twig');
+});
+
+$app->get('/hello/{name}', function($name) use ($app) { 
 #    return 'Hello ' . $app->escape($name); 
     return $app['twig']->render('hello.twig', array(
         'name' => $name,
